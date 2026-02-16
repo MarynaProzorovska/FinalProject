3647800bd8f875f95867f9cbcf78c2f9e60fa79d
MarynaProzorovska <maryna.prozorovska@gmail.com>
2026-02-16 21:53:12 +0100

    test branch 3

--- tests/filters.spec.ts
+++ tests/filters.spec.ts
@@ -6,6 +6,8 @@ test.beforeEach(async ({ headerElements, productPage }) => {
   await expect(productPage.locators.clothesTitle).toBeVisible();
 });
 
+////////////test
+
 test.describe('Verify filters on different pages', () => {
   test('Filters on clothes page', { tag: '@FinalProject03' }, async function async({ filters }) {
     await test.step('Filter by Woman-Category', async function () {
